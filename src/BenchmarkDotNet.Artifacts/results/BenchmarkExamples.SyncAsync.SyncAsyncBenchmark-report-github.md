13aeba5acdfca53f4f42abfe71c9617d119bc5da
Morgan Kenyon <morganwkenyon@gmail.com>
2019-09-15 14:30:55 -0500

    Adding syncAsync benchmark.

--- src/BenchmarkDotNet.Artifacts/results/BenchmarkExamples.SyncAsync.SyncAsyncBenchmark-report-github.md
+++ src/BenchmarkDotNet.Artifacts/results/BenchmarkExamples.SyncAsync.SyncAsyncBenchmark-report-github.md
@@ -8,7 +8,11 @@ Intel Core i7-7700HQ CPU 2.80GHz (Kaby Lake), 1 CPU, 8 logical and 4 physical co
 
 
 ```
-|                 Method |    N |      Mean |     Error |    StdDev |    Median |   Gen 0 | Gen 1 | Gen 2 | Allocated |
-|----------------------- |----- |----------:|----------:|----------:|----------:|--------:|------:|------:|----------:|
-|      TestingSyncMethod | 1000 |  2.937 us | 0.0420 us | 0.0392 us |  2.930 us |  7.6256 |     - |     - |  23.44 KB |
-| TestingSyncAsyncMethod | 1000 | 31.348 us | 1.2063 us | 3.3825 us | 30.310 us | 30.3040 |     - |     - |  93.12 KB |
+|                 Method |     N |       Mean |     Error |     StdDev |     Median |    Gen 0 | Gen 1 | Gen 2 | Allocated |
+|----------------------- |------ |-----------:|----------:|-----------:|-----------:|---------:|------:|------:|----------:|
+|      **TestingSyncMethod** |  **1000** |   **4.282 us** | **0.0938 us** |  **0.2735 us** |   **4.258 us** |   **7.6218** |     **-** |     **-** |  **23.44 KB** |
+| TestingSyncAsyncMethod |  1000 |  35.206 us | 1.0723 us |  1.2348 us |  34.858 us |  30.2734 |     - |     - |  93.12 KB |
+|      **TestingSyncMethod** |  **5000** |  **21.333 us** | **0.5167 us** |  **1.4402 us** |  **20.896 us** |  **38.1165** |     **-** |     **-** | **117.19 KB** |
+| TestingSyncAsyncMethod |  5000 | 182.263 us | 4.4757 us | 12.9847 us | 182.059 us | 152.3438 |     - |     - | 468.12 KB |
+|      **TestingSyncMethod** | **10000** |  **45.021 us** | **0.9646 us** |  **2.7522 us** |  **44.505 us** |  **76.2329** |     **-** |     **-** | **234.38 KB** |
+| TestingSyncAsyncMethod | 10000 | 351.563 us | 6.9928 us | 18.6652 us | 348.298 us | 304.6875 |     - |     - | 936.87 KB |
--- src/BenchmarkExamples/SyncAsync/SyncAsyncBenchmark.cs
+++ src/BenchmarkExamples/SyncAsync/SyncAsyncBenchmark.cs
@@ -1,13 +1,57 @@
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 
 namespace BenchmarkExamples.SyncAsync
 {
+    public class SyncExample
+    {
+        readonly int _number;
+        public SyncExample(int number)
+        {
+            _number = number;
+        }
+
+        public int SyncNumber()
+        {
+            return _number;
+        }
+    }
+
+    public class SyncAsyncExample
+    {
+        readonly int _number;
+        public SyncAsyncExample(int number)
+        {
+            _number = number;
+        }
+
+        public async Task<int> SyncAsyncNumber()
+        {
+            return _number;
+        }
+    }
+
+    public class ValueTaskExample
+    { 
+        readonly int _number;
+        public ValueTaskExample(int number)
+        {
+            _number = number;
+        }
+
+        public async ValueTask<int> ValueTaskNumber()
+        {
+            return _number;
+        }
+    }
+
     [MemoryDiagnoser]
     public class SyncAsyncBenchmark
     {
 
-        [Params(1000)]
+        [Params(1000, 5000, 10000)]
         public int N;
 
 
@@ -33,31 +77,18 @@ namespace BenchmarkExamples.SyncAsync
                 sum += await sync.SyncAsyncNumber();
             }
         }
-    }
 
-    public class SyncExample
-    {
-        readonly int _number;
-        public SyncExample(int number)
-        {
-            number = _number;
-        }
+        //[Benchmark]
+        //public async Task TestingValueTaskMethod()
+        //{
+        //    var sum = 0;
+        //    for (int i = 0; i < N; i++)
+        //    {
+        //        var valueTask = new ValueTaskExample(i);
+        //        sum += await valueTask.ValueTaskNumber();
+        //    }
+        //}
 
-        public int SyncNumber()
-        {
-            return _number;
-        }
-    }
-
-    public class SyncAsyncExample
-    {
-        readonly int _number;
-        public SyncAsyncExample(int number)
-        {
-            _number = number;
-        }
-
-        public async Task<int> SyncAsyncNumber() => _number;
     }
 
 }
