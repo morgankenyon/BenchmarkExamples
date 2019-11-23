fbf63a24c5798689192e63a4aff9b415fb753ac6
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2019-11-23 13:03:58 +0100

    Add more variants of loops with index

--- src/BenchmarkExamples/ForLoop/ForLoopBenchmark.cs
+++ src/BenchmarkExamples/ForLoop/ForLoopBenchmark.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using BenchmarkDotNet.Attributes;
 
 namespace BenchmarkExamples.ForLoop
@@ -13,14 +12,17 @@ namespace BenchmarkExamples.ForLoop
         public int N;
 
         public List<int> intList;
+        public int[] intArray;
 
         [GlobalSetup]
         public void PrepareList()
         {
             intList = new List<int>();
+            intArray = new int[N];
             for(int i = 0; i < N; i++)
             {
                 intList.Add(i);
+                intArray[i] = i;
             }
         }
 
@@ -28,7 +30,6 @@ namespace BenchmarkExamples.ForLoop
         public void RegularForLoop()
         {
             var sum = 0;
-            var intArray = new object[N];
             for (int i = 0; i < intArray.Length; i++)
                 sum++;
         }
@@ -37,7 +38,6 @@ namespace BenchmarkExamples.ForLoop
         public void ForEachLoop()
         {
             var sum = 0;
-            var intArray = new object[N];
             foreach (var i in intArray)
                 sum++;
         }
@@ -46,8 +46,39 @@ namespace BenchmarkExamples.ForLoop
         public void WithIndexLoop()
         {
             var sum = 0;
-            var intArray = new object[N];
             foreach (var (item, index) in intArray.WithIndex())
+                sum++;
+        }
+
+        [Benchmark]
+        public void WithIndexCachedDelegateLoop()
+        {
+            var sum = 0;
+            foreach (var (item, index) in intArray.WithIndexCachedDelegate())
+                sum++;
+        }
+
+        [Benchmark]
+        public void WithIndexIteratorLoop()
+        {
+            var sum = 0;
+            foreach (var (item, index) in intArray.WithIndexIterator())
+                sum++;
+        }
+
+        [Benchmark]
+        public void WithIndexCustomEnumeratorLoop()
+        {
+            var sum = 0;
+            foreach (var (item, index) in intArray.WithIndexCustomEnumerator())
+                sum++;
+        }
+
+        [Benchmark]
+        public void WithIndexSpecificEnumeratorLoopArray()
+        {
+            var sum = 0;
+            foreach (var (item, index) in intArray.WithIndexArray())
                 sum++;
         }
 
@@ -75,6 +106,40 @@ namespace BenchmarkExamples.ForLoop
                 sum++;
         }
 
+
+        [Benchmark]
+        public void WithIndexCachedDelegateLoopList()
+        {
+            var sum = 0;
+            foreach (var (item, index) in intList.WithIndexCachedDelegate())
+                sum++;
+        }
+
+        
+        [Benchmark]
+        public void WithIndexIteratorLoopList()
+        {
+            var sum = 0;
+            foreach (var (item, index) in intList.WithIndexIterator())
+                sum++;
+        }
+
+
+        [Benchmark]
+        public void WithIndexCustomEnumeratorLoopList()
+        {
+            var sum = 0;
+            foreach (var (item, index) in intList.WithIndexCustomEnumerator())
+                sum++;
+        }
+
+        [Benchmark]
+        public void WithIndexSpecificEnumeratorLoopList()
+        {
+            var sum = 0;
+            foreach (var (item, index) in intList.WithIndexList())
+                sum++;
+        }
     }
 
     public static class Extensions
@@ -83,6 +148,152 @@ namespace BenchmarkExamples.ForLoop
         public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source)
         {
             return source.Select((item, index) => (item, index));
+        }
+
+        public static IEnumerable<(T item, int index)> WithIndexCachedDelegate<T>(this IEnumerable<T> source)
+        {
+            return source.Select(Cache<T>.Projection);
+        }
+        
+        private static class Cache<T>
+        {
+            public static readonly Func<T, int, (T, int)> Projection = (item, index) => (item, index);
+        }
+
+        public static IEnumerable<(T item, int index)> WithIndexIterator<T>(this IEnumerable<T> source)
+        {
+            int index = 0;
+            foreach (var item in source)
+            {
+                yield return (item, index);
+                index++;
+            }
+        }
+        
+        public static WithIndexEnumerable<T> WithIndexCustomEnumerator<T>(this IEnumerable<T> source)
+        {
+            return new WithIndexEnumerable<T>(source);
+        }
+        
+        public struct WithIndexEnumerable<T>
+        {
+            private readonly IEnumerable<T> _source;
+            
+            public WithIndexEnumerable(IEnumerable<T> source)
+            {
+                _source = source;
+            }
+            
+            public WithIndexEnumerator<T> GetEnumerator() => new WithIndexEnumerator<T>(_source.GetEnumerator());
+        }
+        
+        public struct WithIndexEnumerator<T>
+        {
+            private readonly IEnumerator<T> _innerEnumerator;
+            private int _index;
+
+            public WithIndexEnumerator(IEnumerator<T> inner)
+            {
+                _innerEnumerator = inner;
+                _index = -1;
+            }
+
+            public bool MoveNext()
+            {
+                if (_innerEnumerator.MoveNext())
+                {
+                    _index++;
+                    return true;
+                }
+                
+                return false;
+            }
+            
+            public (T item, int index) Current => (_innerEnumerator.Current, _index);
+        }
+
+        public static WithIndexListEnumerable<T> WithIndexList<T>(this List<T> source)
+        {
+            return new WithIndexListEnumerable<T>(source);
+        }
+
+        public struct WithIndexListEnumerable<T>
+        {
+            private readonly List<T> _list;
+            
+            public WithIndexListEnumerable(List<T> list)
+            {
+                _list = list;
+            }
+            
+            public Enumerator GetEnumerator() => new Enumerator(_list.GetEnumerator());
+
+            public struct Enumerator
+            {
+                private List<T>.Enumerator _inner;
+                private int _index;
+
+                public Enumerator(List<T>.Enumerator inner)
+                {
+                    _inner = inner;
+                    _index = -1;
+                }
+
+                public bool MoveNext()
+                {
+                    if (_inner.MoveNext())
+                    {
+                        _index++;
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                public (T, int) Current => (_inner.Current, _index);
+            }
+        }
+        
+        public static WithIndexArrayEnumerable<T> WithIndexArray<T>(this T[] source)
+        {
+            return new WithIndexArrayEnumerable<T>(source);
+        }
+        
+        public struct WithIndexArrayEnumerable<T>
+        {
+            private readonly T[] _array;
+            
+            public WithIndexArrayEnumerable(T[] array)
+            {
+                _array = array;
+            }
+            
+            public Enumerator GetEnumerator() => new Enumerator(_array);
+            
+            public struct Enumerator
+            {
+                private readonly T[] _array;
+                private int _index;
+                
+                public Enumerator(T[] array)
+                {
+                    _array = array;
+                    _index = -1;
+                }
+                
+                public bool MoveNext()
+                {
+                    if (_index + 1 < _array.Length)
+                    {
+                        _index++;
+                        return true;
+                    }
+                    
+                    return false;
+                }
+                
+                public (T, int) Current => (_array[_index], _index);
+            }
         }
     }
 }
