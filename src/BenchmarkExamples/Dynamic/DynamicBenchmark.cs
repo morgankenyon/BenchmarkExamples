57981798ff1a4b62358c73674aff3c22848e1afd
rootflood <50250836+rootflood@users.noreply.github.com>
2021-08-16 14:02:10 +0430

    Revert "Update SDK version"
    This reverts commit 32deae16d034b2f3dd4f28a60949cf8cca7033fd.

--- src/BenchmarkExamples/Dynamic/DynamicBenchmark.cs
+++ src/BenchmarkExamples/Dynamic/DynamicBenchmark.cs
@@ -12,86 +12,70 @@ namespace BenchmarkExamples.Dynamic
         [Params(100)]
         public int N;
 
-        [GlobalSetup]
-        public void GlobalS()
+        [Benchmark]
+        public void SumOneToN()
         {
-            StringConcat_V = "";
-            DynamicStringConcat_V = "";
-            FormatUser_V = new DynamicUser("John Billon", "Arlington", 23);
-            DynamicallyFormatUser_V = FormatUser_V;
-            StructFormatUser_V = new DynamicStruct();
-            StructDynamicallyFormatUser_V = StructFormatUser_V;
+            var sum = 0;
+            for(int i = 0; i < N; i++)
+                sum += i;
         }
 
-        private string StringConcat_V;
         [Benchmark]
-        public object StringConcat()
+        public void StringConcat()
         {
-            var StringConcat_V = this.StringConcat_V;
+            string concated = "";
             string message = "Message";
             for (int i = 0; i < N; i++)
-                StringConcat_V = this.StringConcat_V + StringConcat_V;
-            return StringConcat_V;
+                concated = message + concated.ToString();
         }
 
-        private dynamic DynamicStringConcat_V;
         [Benchmark]
-        public object DynamicStringConcat()
+        public void DynamicStringConcat()
         {
-            var DynamicStringConcat_V = this.DynamicStringConcat_V;
+            dynamic concated = "";
             string message = "Message";
             for (int i = 0; i < N; i++)
-                DynamicStringConcat_V = this.DynamicStringConcat_V + DynamicStringConcat_V;
-            return DynamicStringConcat_V;
+                concated = message + concated.ToString();
         }
 
-
-        public DynamicUser FormatUser_V;
         [Benchmark]
-        public object FormatUser()
+        public void FormatUser()
         {
-            var FormatUser_V = this.FormatUser_V;
             for (int i = 0; i < N; i++)
             {
-                FormatUser_V.UpdateName("Crazy");
+                var user = new DynamicUser("John Billon", "Arlington", 23);
+                user.UpdateName("Crazy");
             }
-            return FormatUser_V;
         }
 
-        private dynamic DynamicallyFormatUser_V;
         [Benchmark]
-        public object DynamicallyFormatUser()
+        public void DynamicallyFormatUser()
         {
-            var DynamicallyFormatUser_V = this.DynamicallyFormatUser_V;
             for (int i = 0; i < N; i++)
             {
-                DynamicallyFormatUser_V.UpdateName("Crazy");
+                dynamic user = new DynamicUser("John Billon", "Arlington", 23);
+                user.UpdateName("Crazy");
             }
-            return DynamicallyFormatUser_V;
         }
 
-        private DynamicStruct StructFormatUser_V;
         [Benchmark]
-        public object StructFormatUser()
+        public void StructFormatUser()
         {
-            var StructFormatUser_V = this.StructFormatUser_V;
             for (int i = 0; i < N; i++)
             {
-                StructFormatUser_V.UpdateName("Crazy");
+                var user = new DynamicStruct("John Billon", "Arlington", 23);
+                user.UpdateName("Crazy");
             }
-            return StructFormatUser_V;
         }
 
-        public dynamic StructDynamicallyFormatUser_V;
         [Benchmark]
-        public object StructDynamicallyFormatUser()
+        public void StructDynamicallyFormatUser()
         {
-            var StructDynamicallyFormatUser_V = this.StructDynamicallyFormatUser_V;
             for (int i = 0; i < N; i++)
             {
-                StructDynamicallyFormatUser_V.UpdateName("Crazy");
+                dynamic user = new DynamicStruct("John Billon", "Arlington", 23);
+                user.UpdateName("Crazy");
             }
-            return StructDynamicallyFormatUser_V;
         }
     }
 }
